314edf88a15e1e4bcee482691c6804fee19c5a55
Adam <adam.gorzelak3@outlook.com>
2022-08-12 20:17:47 +0200

    fixed nav

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', function () {
 	const navLink = document.querySelectorAll('.nav-link')
 	const navList = document.querySelector('.navbar-collapse')
 
-	navLink.forEach(link => link.addEventListener('click', () => navList.classList.remove('show')))
+	navLink.forEach(link => link.addEventListener('click', () => navList.classList.toggle('show')))
 })
